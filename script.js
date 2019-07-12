fdce83a078a79b23808c029cd40d78068aac9eb4
lavylipesh <lilibethlangat@yahoo.com>
2019-07-12 17:42:22 -0400

    add details to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var days = [ "Sunday" ,   "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+
+
