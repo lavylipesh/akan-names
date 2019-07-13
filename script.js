49ace49224fbef535a9127e7d1141971a840e936
lavylipesh <lilibethlangat@yahoo.com>
2019-07-13 12:21:32 -0400

    add changes

--- script.js
+++ script.js
@@ -1,6 +1,75 @@
-var days = [ "Sunday" ,   "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var day = [ "Sunday" ,   "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var CC= document.getElementById("CC").value = number;
+var YY=document.getElementById("YY").value = number;
+var MM=document.getElementById("MM").value = number;
+var DD=document.getElementById("DD").value = number;
+
+var enter=document.getElementById("gender").value = number;
+
+
+
+
+
+
+
+
+
+
+
+
+
+var calculate=function workout (){ 
+return workout (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7
+}
+var male=true;
+if(day===0){
+    console.log("you were born on a Sunday  your akan name is Kwasi")
+}
+else if(day===1){
+    console.log("you were born on a Monday your akan name is Kwadwo")
+}
+ else if( day===2){
+     console.log("you were born on a Tuesday your akan name is Kwabena")
+ }
+ else if(day===3){
+    console.log("you were born on a Wednesday your akan name is Kwaku")
+}
+ else if(day===4){
+     console.log("you were born on a Thursday your akan name is Yaw")
+ }
+ else if(day===5){
+    console.log("you were born on a Friday your akan name is Kofi")
+}
+else if(day===6){
+   console.log("you were boorn on a Saturday your akan name is Kwame")
+}
+
+var female=true;
+if(day===0){
+    console.log("you were born on a Sunday  your akan name is Akosua")
+}
+else if(day===1){
+    console.log("you were born on a Monday your akan name is Adwoa")
+}
+ else if(day===2){
+     console.log("you were born on a Tuesday your akan name is Abenaa")
+ }
+ else if(day===3){
+    console.log("you were born on a Wednesday your akan name is Akua")
+}
+ else if(day===4){
+     console.log("you were born on a Thursday your akan name is Yaa")
+ }
+ else if(day===5){
+    console.log("you were born on a Friday your akan name is Afua")
+}
+else if(day===6){
+   console.log(("you were boorn on a Saturday your akan name Ama")
+}
+
+
 
 
 
